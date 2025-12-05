e4da0c53714442c19d5935d6eb925062531c1db4
kashik09 <kashiku789@gmail.com>
2025-12-05 17:28:10 +0300

    Add capitalizeWords edge case tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,3 +3,15 @@ const { capitalizeWords, filterActiveUsers, logAction } = require('../index')
 test('capitalizeWords handles normal input', () => {
   expect(capitalizeWords('hello world')).toBe('Hello World');
 });
+
+test('capitalizeWords handles empty string', () => {
+  expect(capitalizeWords('')).toBe('');
+});
+
+test('capitalizeWords handles special characters', () => {
+  expect(capitalizeWords('hello-world')).toBe('Hello-World');
+});
+
+test('capitalizeWords handles single word', () => {
+  expect(capitalizeWords('hello')).toBe('Hello');
+});
