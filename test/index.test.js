fc34bd4d4b9bd186875e323f0b03f7ca09f57750
kashik09 <kashiku789@gmail.com>
2025-12-05 17:27:48 +0300

    Add capitalizeWords normal case test

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,5 @@
 const { capitalizeWords, filterActiveUsers, logAction } = require('../index')
 
+test('capitalizeWords handles normal input', () => {
+  expect(capitalizeWords('hello world')).toBe('Hello World');
+});
